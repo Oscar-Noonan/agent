5299cde7f6c26a43c9a57537cc6ba116a08f0674
Oscar-Noonan <oscar.noonan13@gmail.com>
2026-08-07 21:39:34 +0100

    feature/openrouter-api-connects-to-api-provider

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ try:
     api_key = os.environ.get("OPENROUTER_API_KEY")
 except:
     raise Exception("environment variable not found")
+
+try:
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key,
+    )
+except:
+    raise Exception ("cannot connect to API provider")
+    
