85b86b99b65ad8f8f785d378fc008987057b0aa7
Oscar-Noonan <oscar.noonan13@gmail.com>
2026-08-12 19:12:46 +0100

    feature/list-files-fuction-to-list-file-info-from-requested-filepath

--- functions.py
+++ functions.py
@@ -1,18 +1,31 @@
 import os
 
-def get_files_info(working_dir: str, dir: str = ".") -> str:
-    # gets the absolute filepath of working_dir
-    abs_working_dir = os.path.abspath(working_dir)
+def get_files_info(working_directory: str, directory: str = ".") -> str:
+    # gets the absolute filepath of working_directory
+    abs_working_directory = os.path.abspath(working_directory)
 
-    # construct the full path to dir
-    target_dir = os.path.normpath(os.path.join(abs_working_dir, dir))
+    # construct the full path to directory
+    target_directory = os.path.normpath(os.path.join(abs_working_directory, directory))
 
-    # check if target_dir is in abs_working_dir
-    valid_target_dir = os.path.commonpath([abs_working_dir, target_dir]) == abs_working_dir
+    # check if target_directory is in abs_working_directory
+    valid_target_directory = os.path.commonpath([abs_working_directory, target_directory]) == abs_working_directory
 
-    if not valid_target_dir:
-        return f'Error: Cannot list "{dir}" as it is outside the permitted working directory'
-    elif not os.path.isdir(dir):
-        return f'Error: "{dir}" is not a directory'
-    else:
-        return f'Success: "{dir}" is within the working directory'
+    result: str = f"Result for '{directory}' directory:"
+
+
+    if not valid_target_directory:
+        result += f'\n   Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        return result
+
+    if not os.path.exists(target_directory) or not os.path.isdir(target_directory):
+        result += f'\n   Error: "{directory}" is not a directory'
+        return result
+    
+
+    items = os.listdir(target_directory)
+
+    for item in items:
+        item_path = os.path.join(target_directory, item)
+        result += f"\n    - {item}: file_size={os.path.getsize(item_path)}, is_dir={os.path.isdir(item_path)}"
+    
+    return result
